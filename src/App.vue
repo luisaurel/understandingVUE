3d2afc3e708c1cbf71b641917ad7fc04fe3601a7
luisaurel <luisaurel.kraus@icloud.com>
2025-09-15 14:02:10 +0200

    WARUM GEHT DAS STYLING NICHT

--- src/App.vue
+++ src/App.vue
@@ -43,8 +43,33 @@ import ProvideInjectDemo from './components/ProvideInjectDemo.vue'
 const sharedCount = ref(0)
 </script>
 
-<style scoped> 
-/* Diesen Code in <style scoped> in der Datei src/App.vue einfügen */
+<style scoped>
+header {
+  line-height: 1.5;
+}
+
+.logo {
+  display: block;
+  margin: 0 auto 2rem;
+}
+
+@media (min-width: 1024px) {
+  header {
+    display: flex;
+    place-items: center;
+    padding-right: calc(var(--section-gap) / 2);
+  }
+
+  .logo {
+    margin: 0 2rem 0 0;
+  }
+
+  header .wrapper {
+    display: flex;
+    place-items: flex-start;
+    flex-wrap: wrap;
+  }
+}
 
 .welcome-item {
   margin-top: 2rem;
@@ -117,4 +142,3 @@ h3 {
   }
 }
 </style>
-
