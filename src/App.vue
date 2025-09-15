1d88c9988b399e0622d73057a47d96d75cebabba
luisaurel <luisaurel.kraus@icloud.com>
2025-09-15 13:52:30 +0200

    style in App.vue, aber funktioniert nicht

--- src/App.vue
+++ src/App.vue
@@ -43,7 +43,78 @@ import ProvideInjectDemo from './components/ProvideInjectDemo.vue'
 const sharedCount = ref(0)
 </script>
 
-<style>
-#app { font-family: Arial, sans-serif; padding: 1rem }
-button { margin-left: 0.5rem }
+<style scoped> 
+/* Diesen Code in <style scoped> in der Datei src/App.vue einfügen */
+
+.welcome-item {
+  margin-top: 2rem;
+  display: flex;
+  position: relative;
+}
+
+.details {
+  flex: 1;
+  margin-left: 1rem;
+}
+
+i {
+  display: flex;
+  place-items: center;
+  place-content: center;
+  width: 32px;
+  height: 32px;
+  color: var(--color-text);
+}
+
+h3 {
+  font-size: 1.2rem;
+  font-weight: 500;
+  margin-bottom: 0.4rem;
+  color: var(--color-heading);
+}
+
+@media (min-width: 1024px) {
+  .welcome-item {
+    margin-top: 0;
+    padding: 0.4rem 0 1rem calc(var(--section-gap) / 2);
+  }
+
+  i {
+    top: calc(50% - 25px);
+    left: -26px;
+    position: absolute;
+    border: 1px solid var(--color-border);
+    background: var(--color-background);
+    border-radius: 8px;
+    width: 50px;
+    height: 50px;
+  }
+
+  .welcome-item:before {
+    content: ' ';
+    border-left: 1px solid var(--color-border);
+    position: absolute;
+    left: 0;
+    bottom: calc(50% + 25px);
+    height: calc(50% - 25px);
+  }
+
+  .welcome-item:after {
+    content: ' ';
+    border-left: 1px solid var(--color-border);
+    position: absolute;
+    left: 0;
+    top: calc(50% + 25px);
+    height: calc(50% - 25px);
+  }
+
+  .welcome-item:first-of-type:before {
+    display: none;
+  }
+
+  .welcome-item:last-of-type:after {
+    display: none;
+  }
+}
 </style>
+
